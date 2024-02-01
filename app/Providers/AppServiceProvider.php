eaf43127d221a621f867c3a92fb1c5980180a702
Hudson Fonseca <devhudsonfonseca@gmail.com>
2024-02-01 16:30:01 -0300

    add cj

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     { 
+        Schema::defaultStringLength(191);
+        
         if (\Schema::hasTable('smtp_settings')) {
 
             $smtpsetting = SmtpSetting::first();
